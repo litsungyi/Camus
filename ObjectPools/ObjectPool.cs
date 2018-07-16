0ba725fc0ff217e3c3fdcdc40755ed25dffc7563
litsungyi <litsungyi@gmail.com>
2018-07-17 04:54:14 +0800

    Add simple ObjectPool

---
+++ ObjectPools/ObjectPool.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Camus.ObjectPools
+{
+    public class ObjectPool : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject prefab;
+
+        [SerializeField]
+        private int minInstance;
+
+        private Stack<GameObject> reusables = new Stack<GameObject>();
+
+        private void Awake()
+        {
+            for (int i = 0; i < minInstance; ++i)
+            {
+                var instance = CreateInstance();
+                Release(instance);
+            }
+        }
+
+        private GameObject CreateInstance()
+        {
+            var instance = GameObject.Instantiate(prefab, transform);
+            return instance;
+        }
+
+        public GameObject Require()
+        {
+            var instance = reusables.Any() ? reusables.Pop() : CreateInstance();
+            instance.SetActive(true);
+            return instance;
+        }
+
+        public void Release(GameObject instance)
+        {
+            instance.SetActive(false);
+            reusables.Push(instance);
+        }
+    }
+}
