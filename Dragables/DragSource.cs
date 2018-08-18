4277c74c902ba76d72ce58c98a0d09c2d08b2f56
litsungyi <litsungyi@gmail.com>
2018-08-19 02:41:22 +0800

    Add dragables

---
+++ Dragables/DragSource.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Camus.Dragables
+{
+    public class DragSource : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        public static GameObject dragItem;
+        private Vector3 startPosition;
+
+        #region IBeginDragHandler
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            Debug.Log("OnBeginDrag");
+            dragItem = gameObject;
+            startPosition = transform.position;
+            //GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+
+        #endregion
+
+        #region IDragHandler
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            //Debug.Log("OnDrag");
+            transform.position = Input.mousePosition;
+        }
+
+        #endregion
+
+        #region IEndDragHandler
+
+       public void OnEndDrag(PointerEventData eventData)
+        {
+            Debug.Log("OnEndDrag");
+
+            dragItem = null;
+            //transform.position = startPosition;
+        }
+
+        #endregion
+    }
+}
---
+++ Dragables/DragTarget.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Camus.Dragables
+{
+    public class DragTarget : MonoBehaviour, IDropHandler
+    {
+        private GameObject item;
+
+        #region IDropHandler
+
+        void IDropHandler.OnDrop(PointerEventData eventData)
+        {
+            Debug.Log("OnDrop " + name);
+            
+            if(item == null)
+            {
+                DragSource.dragItem.transform.SetParent(transform);
+            }
+        }
+
+        #endregion
+    }
+}
