990030322de95180b4fac72930626ba0cfae4b3d
litsungyi <litsungyi@gmail.com>
2018-08-31 19:13:51 +0800

    Add timers

---
+++ Timers/TimerBase.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+
+namespace Camus.Timers
+{
+    [Serializable]
+    public class TimerBase
+    {
+        [Header("Display")]
+        public string name;
+        private MonoBehaviour host;
+
+        [Header("Timer Setting")]
+        public float interval;
+        public bool loop;
+        public float maxCount;
+
+        [Header("Timer Setting")]
+        [SerializeField] protected float duration;
+        [SerializeField] protected float count;
+
+        internal void Init(MonoBehaviour host)
+        {
+            this.host = host;
+        }
+
+        internal void StartTimer()
+        {
+            SetEnable(true);
+            ResetTimer();
+        }
+
+        internal void StopTimer()
+        {
+            SetEnable(false);
+            ResetTimer();
+        }
+
+        internal void PauseTimer()
+        {
+            SetEnable(false);
+        }
+
+        internal void ResumeTimer()
+        {
+            if (count >= maxCount && !loop)
+            {
+                return;
+            }
+
+            SetEnable(true);
+        }
+
+        internal void ResetTimer()
+        {
+            duration = 0;
+            count = 0;
+        }
+
+        internal bool Tick(float delta)
+        {
+            duration += delta;
+            if (duration < interval)
+            {
+                return false;
+            }
+
+            duration -= interval;
+            if (++count >= maxCount && !loop)
+            {
+                SetEnable(false);
+            }
+
+            return true;
+        }
+
+        private void SetEnable(bool enabled)
+        {
+            host.enabled = enabled;
+        }
+    }
+}
---
+++ Timers/TimerFixedUpdater.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace Camus.Timers
+{
+    public class TimerFixedUpdater : MonoBehaviour
+    {
+        [SerializeField] private TimerBase setting;
+        private Action onTimer = null;
+
+        private void Awake()
+        {
+            setting.Init(this);
+        }
+
+        private void FixedUpdate()
+        {
+            if (setting.Tick(Time.fixedDeltaTime))
+            {
+                Debug.Log("FixedUpdate");
+                onTimer?.Invoke();
+            }
+        }
+
+        public void StartTimer()
+        {
+            setting.StartTimer();
+        }
+
+        public void StopTimer()
+        {
+            setting.StopTimer();
+        }
+
+        public void PauseTimer()
+        {
+            setting.PauseTimer();
+        }
+
+        public void ResumeTimer()
+        {
+            setting.ResumeTimer();
+        }
+
+        public void ResetTimer()
+        {
+            setting.ResetTimer();
+        }
+    }
+}
