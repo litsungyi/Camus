1d61f9795549af9123d9ece644046503896f4884
litsungyi <litsungyi@gmail.com>
2018-08-31 19:11:18 +0800

    Rename localization to localizables

---
+++ Localizables/LocalStringUpdater.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Camus.Localizables
+{
+    public class LocalStringUpdater : MonoBehaviour
+    {
+        [SerializeField] private Text Text = null;
+        [SerializeField] private string Key = string.Empty;
+
+        private LocalKey localKey;
+
+        private void Start()
+        {
+            if (Text == null || string.IsNullOrEmpty(Key))
+            {
+                return;
+            }
+
+            //localKey = LocalKey.Create(Key);
+            //Text.text = localKey.Value;
+        }
+
+        private void Update()
+        {
+            if (Text == null || string.IsNullOrEmpty(Key))
+            {
+                return;
+            }
+
+            Text.text = localKey.Value;
+        }
+    }
+}
