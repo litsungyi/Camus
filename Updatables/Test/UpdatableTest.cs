13060874110b6a1c749261fa9360e14d60ffab0b
litsungyi <litsungyi@gmail.com>
2018-09-06 00:43:44 +0800

    Add updatable test
    - Call Update/FixedUpdate/LateUpdate in single point, not every gameobject, can improve performance.
    	- 10000 IUpdatable: FPS 40~43
    	- 10000 MonoBehaviour: FPS 33~36

---
+++ Updatables/Test/UpdatableTest.cs
@@ -0,0 +1,12 @@
+﻿using Camus.Updatables;
+using UnityEngine;
+
+public class UpdatableTest : MonoBehaviour, IUpdatable
+{
+    void IUpdatable.OnUpdate(float duration)
+    {
+        //Debug.Log("OnUpdate");
+        var localPosition = transform.localPosition;
+        transform.localPosition = new Vector3(localPosition.x + Random.value, localPosition.y + Random.value, localPosition.z);
+    }
+}
---
+++ Updatables/Test/UpdateTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class UpdateTest : MonoBehaviour
+{
+    private void Update()
+    {
+        //Debug.Log("Update");
+        var localPosition = transform.localPosition;
+        transform.localPosition = new Vector3(localPosition.x + Random.value, localPosition.y + Random.value, localPosition.z);
+    }
+}
