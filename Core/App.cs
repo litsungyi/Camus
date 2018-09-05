3cf864a87bf0379f1996619b8ceb64ae3c725c7f
litsungyi <litsungyi@gmail.com>
2018-09-06 00:39:30 +0800

    Add FPS display in editor mode

--- Core/App.cs
+++ Core/App.cs
@@ -1,6 +1,28 @@
 using Camus.Core;
+using Camus.Utilities;
 
 // NOTE: Use global partial singleton for Game extends.
 public partial class App : Singleton<App>
 {
+    private FPS fps;
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public void EnableFps(bool enable)
+    {
+        if (enable && fps == null)
+        {
+            fps = gameObject.AddComponent<FPS>();
+        }
+        else if (fps != null)
+        {
+            fps.gameObject.SetActive(enable);
+        }
+    }
+
+    public void Awake()
+    {
+#if UNITY_EDITOR
+        EnableFps(true);
+#endif
+    }
 }
---
+++ Utilities/FPS.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Camus.Utilities
+{
+    public class FPS : MonoBehaviour
+    {
+        private static readonly int MaxCount = 100;
+
+        [SerializeField] private int count = 0;
+        [SerializeField] private float frames = 0f;
+        private Queue<float> deltas = new Queue<float>();
+
+        private void Update()
+        {
+            var delta = Time.deltaTime;
+            ++count;
+            deltas.Enqueue(delta);
+
+            if (deltas.Count > MaxCount)
+            {
+                deltas.Dequeue();
+                count = MaxCount;
+            }
+
+            var total = 0f;
+            foreach (var item in deltas)
+            {
+                total += item;
+            }
+
+            var average = count == -1 ? 0 : total / count;
+            frames = average <= 0 ? 0 : 1f / average;
+        }
+
+#if UNITY_EDITOR
+        private void OnGUI()
+        {
+            UnityEditor.EditorGUILayout.LabelField(string.Format("FPS: {0:00.0}", frames));
+        }
+#endif
+    }
+}
