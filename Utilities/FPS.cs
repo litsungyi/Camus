bfddb3d8a351c4d9c1195e9023b246e0fd604e8d
litsungyi <litsungyi@gmail.com>
2018-09-06 03:41:15 +0800

    Fix FPS and update FPS display

--- Utilities/FPS.cs
+++ Utilities/FPS.cs
@@ -7,20 +7,17 @@ namespace Camus.Utilities
     {
         private static readonly int MaxCount = 100;
 
-        [SerializeField] private int count = 0;
         [SerializeField] private float frames = 0f;
         private Queue<float> deltas = new Queue<float>();
 
         private void Update()
         {
             var delta = Time.deltaTime;
-            ++count;
             deltas.Enqueue(delta);
 
             if (deltas.Count > MaxCount)
             {
                 deltas.Dequeue();
-                count = MaxCount;
             }
 
             var total = 0f;
@@ -29,14 +26,40 @@ namespace Camus.Utilities
                 total += item;
             }
 
-            var average = count == -1 ? 0 : total / count;
+            var average = deltas.Count == 0 ? 0 : total / deltas.Count;
             frames = average <= 0 ? 0 : 1f / average;
         }
 
 #if UNITY_EDITOR
         private void OnGUI()
         {
-            UnityEditor.EditorGUILayout.LabelField(string.Format("FPS: {0:00.0}", frames));
+            var style = new GUIStyle(GUI.skin.label);
+            style.normal.textColor = GetColor();
+            style.fontSize = 16;
+            style.alignment = TextAnchor.MiddleRight;
+
+            var rect = new Rect(new Vector2(Screen.width - 110, 10), new Vector2(100, 20));
+            UnityEditor.EditorGUI.LabelField(rect, string.Format("FPS: {0:00.0}", frames), style);
+        }
+
+        private Color GetColor()
+        {
+            if (frames >= 60f)
+            {
+                return Color.green;
+            }
+            else if (frames >= 30f)
+            {
+                return Color.blue;
+            }
+            else if (frames >= 10f)
+            {
+                return Color.yellow;
+            }
+            else
+            {
+                return Color.red;
+            }
         }
 #endif
     }
