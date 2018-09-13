c1c088be7f84de5fc12735c377c220fd43b191f6
litsungyi <litsungyi@gmail.com>
2018-09-13 19:34:33 +0800

    Add Ids

---
+++ Core/Ids/Identity.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Camus.Core.Ids
+{
+    /// <summary>
+    /// 用來作為識別碼的類別
+    /// 目前只支援 int, uint, long, ulong, string
+    /// </summary>
+    /// <typeparam name="T">實際的識別碼型別</typeparam>
+    [Serializable]
+    [JsonConverter(typeof(IdentitySerializer))]
+    public abstract class Identity<T>
+    {
+        /// <summary>
+        /// 建構識別碼
+        /// </summary>
+        /// <param name="id">唯一識別碼</param>
+        public Identity(T id)
+        {
+            this.id = id;
+        }
+
+        [SerializeField] private T id;
+
+        /// <summary>
+        /// 唯一識別碼
+        /// </summary>
+        public T Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        #region Equal
+        public static bool operator ==(Identity<T> lhs, Identity<T> rhs)
+        {
+            if (object.ReferenceEquals(lhs, rhs))
+            {
+                return true;
+            }
+
+            // If one is null, but not both, return false.
+            if (((object) lhs == null) || ((object) rhs == null))
+            {
+                return false;
+            }
+
+            if (lhs.Id == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return lhs.Id.Equals(rhs.Id);
+        }
+
+        public static bool operator !=(Identity<T> lhs, Identity<T> rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public override bool Equals(object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            Identity<T> that = obj as Identity<T>;
+            if ((object) that == null)
+            {
+                return false;
+            }
+
+            if (this.Id == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return this.Id.Equals(that.Id);
+        }
+
+        public bool Equals(Identity<T> that)
+        {
+            if ((object) that == null)
+            {
+                return false;
+            }
+
+            if (this.Id == null)
+            {
+                return false;
+            }
+
+            return this.Id.Equals(that.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.Id.ToString();
+        }
+        #endregion
+    }
+}
---
+++ Core/Ids/IdentitySerializer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace Camus.Core.Ids
+{
+    /// <summary>
+    /// 對 Identity 作 Jsonlization 的轉換類別
+    /// 將繼承 Identity<int> 的類別轉成 "Id" = 0
+    /// 將繼承 Identity<string> 的類別轉成 "Id" = "0"
+    /// </summary>
+    public class IdentitySerializer : JsonConverter
+    {
+        #region JsonConverter
+        public override bool CanConvert( Type objectType )
+        {
+            return true;
+        }
+
+        public override object ReadJson( JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer )
+        {
+            Type baseType = objectType.BaseType;
+            if ( !baseType.IsGenericType || baseType.GetGenericTypeDefinition() != typeof( Identity<> ) )
+            {
+                throw new Exception( "XxxId must be sealed and inheritance from Identity<>" );
+            }
+
+            Type idType = baseType.GetGenericArguments()[ 0 ];
+            if ( idType == typeof( string ) )
+            {
+                string id = ReadStringId( reader.Value );
+                return Activator.CreateInstance( objectType, id );
+            }
+            else
+            {
+                MethodInfo methodInfo = typeof( IdentitySerializer ).GetMethod( "ReadIntegerId", BindingFlags.Static | BindingFlags.NonPublic );
+                methodInfo = methodInfo.MakeGenericMethod( new Type[] { idType } );
+                var parameters = new object[] { reader.Value };
+                var id = methodInfo.Invoke( null, parameters );
+                return Activator.CreateInstance( objectType, id );
+            }
+        }
+
+        public override void WriteJson( JsonWriter writer, object value, JsonSerializer serializer )
+        {
+            Type baseType = value.GetType().BaseType;
+            if ( !baseType.IsGenericType || baseType.GetGenericTypeDefinition() != typeof( Identity<> ) )
+            {
+                throw new Exception( "XxxId must be sealed and inheritance from Identity<>" );
+            }
+
+            Type idType = baseType.GetGenericArguments()[ 0 ];
+            MethodInfo methodInfo = typeof( IdentitySerializer ).GetMethod( "GetId", BindingFlags.Static | BindingFlags.NonPublic );
+            methodInfo = methodInfo.MakeGenericMethod( new Type[] { idType } );
+            var parameters = new object[] { value };
+            var id = methodInfo.Invoke( null, parameters );
+            serializer.Serialize( writer, id );
+        }
+        #endregion
+
+        #region private
+        private static T GetId<T>( Identity<T> identity )
+        {
+            return identity.Id;
+        }
+
+        private static string ReadStringId( object value )
+        {
+            return ( string ) value;
+        }
+
+        private static TType ReadIntegerId<TType>( object value )
+        {
+            if ( value == null )
+            {
+                return default( TType );
+            }
+
+            return ( TType ) Convert.ChangeType( value.ToString(), typeof( TType ) );
+        }
+        #endregion
+    }
+}
