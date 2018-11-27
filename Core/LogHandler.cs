3bd5149309c87d68499e51f087e5526320af2e2a
litsungyi <litsungyi@gmail.com>
2018-11-28 07:47:54 +0800

    Add Log (WIP)

---
+++ Core/LogHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Camus.Core
+{
+    public class LogHandler : ILogHandler
+    {
+        private ILogHandler defaultLogHandler = Debug.unityLogger.logHandler;
+
+        public LogHandler()
+        {
+            Debug.unityLogger.logHandler = this;
+        }
+
+        public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
+        {
+            defaultLogHandler.LogFormat(logType, context, format, args);
+        }
+
+        public void LogException(Exception exception, UnityEngine.Object context)
+        {
+            defaultLogHandler.LogException(exception, context);
+        }
+    }
+}
---
+++ Core/Logger.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System;
+
+namespace Camus.Core
+{
+    public static class Logger
+    {
+        private static readonly string EmptyTag = string.Empty;
+
+        private static ILogHandler logHandler = new LogHandler();
+
+        #region Log
+
+        public statuc void Log(string message)
+        {
+            logHandler.LogFormat(message);
+        }
+
+        public static void Log(string tag, string message)
+        {
+            logHandler.Log($"[{tag}] {message}");
+        }
+
+        public static void Log(string tag, string message, UnityEngine.Object context)
+        {
+            logHandler.Log($"[{tag}] {message}", context);
+        }
+
+        #endregion
+
+        #region LogWarning
+
+        public static void LogWarning(string tag, string message)
+        {
+            logHandler.LogWarning($"[{tag}] {message}");
+        }
+
+        public static void ILogger.LogWarning(string tag, string message, UnityEngine.Object context)
+        {
+            logHandler.LogWarning($"[{tag}] {message}", context);
+        }
+
+        #endregion
+
+        #region LogError
+
+        public static void ILogger.LogError(string tag, string message)
+        {
+            logHandler.LogError($"[{tag}] {message}");
+        }
+
+        public static void ILogger.LogError(string tag, object message, UnityEngine.Object context)
+        {
+            logHandler.LogError($"[{tag}] {message}", context);
+        }
+
+        #endregion
+
+        #region LogException
+
+        public static void ILogger.LogException(Exception exception)
+        {
+            logHandler.LogException(exception);
+        }
+
+        public static void ILogHandler.LogException(Exception exception, UnityEngine.Object context)
+        {
+            logHandler.LogException(exception, context);
+        }
+
+        #endregion
+    }
+}
