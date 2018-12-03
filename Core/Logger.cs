500150aa6f85777eb96c5690d6d3efb04ecb2375
litsungyi <litsungyi@gmail.com>
2018-12-04 04:01:21 +0800

    Update logger

--- Core/Logger.cs
+++ Core/Logger.cs
@@ -1,71 +1,68 @@
-﻿using UnityEngine;
-using System;
+﻿using System;
+using UnityEngine;
 
 namespace Camus.Core
 {
     public static class Logger
     {
-        private static readonly string EmptyTag = string.Empty;
-
         private static ILogHandler logHandler = new LogHandler();
 
         #region Log
 
-        public statuc void Log(string message)
+        public static void Log(string message, UnityEngine.Object context = null)
         {
-            logHandler.LogFormat(message);
+            DoLog(LogType.Log, context, message);
         }
 
-        public static void Log(string tag, string message)
+        public static void Log<T>(T tag, string message, UnityEngine.Object context = null) where T : struct
         {
-            logHandler.Log($"[{tag}] {message}");
-        }
-
-        public static void Log(string tag, string message, UnityEngine.Object context)
-        {
-            logHandler.Log($"[{tag}] {message}", context);
+            Log($"[{tag.ToString()}] {message}", context);
         }
 
         #endregion
 
         #region LogWarning
 
-        public static void LogWarning(string tag, string message)
+        public static void LogWarning(string message, UnityEngine.Object context = null)
         {
-            logHandler.LogWarning($"[{tag}] {message}");
+            DoLog(LogType.Warning, context, message);
         }
 
-        public static void ILogger.LogWarning(string tag, string message, UnityEngine.Object context)
+        public static void LogWarning<T>(T tag, string message, UnityEngine.Object context = null) where T : struct
         {
-            logHandler.LogWarning($"[{tag}] {message}", context);
+            LogWarning($"[{tag.ToString()}] {message}", context);
         }
 
         #endregion
 
         #region LogError
 
-        public static void ILogger.LogError(string tag, string message)
+        public static void LogError(string message, UnityEngine.Object context = null)
         {
-            logHandler.LogError($"[{tag}] {message}");
+            DoLog(LogType.Error, context, message);
         }
 
-        public static void ILogger.LogError(string tag, object message, UnityEngine.Object context)
+        public static void LogError<T>(T tag, string message, UnityEngine.Object context = null) where T : struct
         {
-            logHandler.LogError($"[{tag}] {message}", context);
+            LogError($"[{tag.ToString()}] {message}", context);
         }
 
         #endregion
 
         #region LogException
 
-        public static void ILogger.LogException(Exception exception)
-        {
-            logHandler.LogException(exception);
-        }
-
-        public static void ILogHandler.LogException(Exception exception, UnityEngine.Object context)
+        public static void LogException(Exception exception, UnityEngine.Object context = null)
         {
             logHandler.LogException(exception, context);
+        }
+
+        #endregion
+
+        #region Detail
+
+        private static void DoLog(LogType logType, UnityEngine.Object context, string message)
+        {
+            logHandler.LogFormat(logType, context, message);
         }
 
         #endregion
