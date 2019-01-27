3609fa47a033343e66da1733c9877fa2645e732b
litsungyi <litsungyi@gmail.com>
2019-01-28 07:13:11 +0800

    Add SceneController

---
+++ Core/App.cs
@@ -0,0 +1,56 @@
+using System;
+using Camus;
+using Camus.Core;
+using Camus.Utilities;
+
+// NOTE: Use global partial singleton for Game extends.
+public partial class App : Singleton<App>
+{
+    private enum LogTag
+    {
+        HIGH,
+        LOW,
+        MIDDLE,
+    }
+
+    private FPS fps;
+
+
+
+    private SceneController sceneController;
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public void EnableFps(bool enable)
+    {
+        if (enable && fps == null)
+        {
+            fps = gameObject.AddComponent<FPS>();
+        }
+        else if (fps != null)
+        {
+            fps.gameObject.SetActive(enable);
+        }
+    }
+
+    public void Awake()
+    {
+#if UNITY_EDITOR
+        EnableFps(true);
+#endif
+
+        Logger.Log("App Awake");
+        Logger.LogWarning("App Awake LogWarning");
+        Logger.LogError("App Awake LogError");
+        Logger.Log(LogTag.HIGH, "App Awake");
+        Logger.LogWarning(LogTag.LOW, "App Awake LogWarning");
+        Logger.LogError(LogTag.MIDDLE, "App Awake LogError");
+        try
+        {
+            throw new Exception("Error", new Exception("Inner"));
+        }
+        catch (Exception ex)
+        {
+            Logger.LogException(ex);
+        }
+    }
+}
---
+++ Scenes/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Camus
+{
+    public class SceneController
+    {
+
+
+        //public IEnumerator LoadSceneAsync(int sceneIndex)
+        //{
+            //var currentScene = SceneManager.GetActiveScene();
+            //
+            //
+            //SceneManager.LoadSceneAsync(sceneIndex);
+            //var loading = SceneManager.LoadSceneAsync("Game");
+            //while (!loading.isDone)
+            //{
+            //    yield return null;
+            //}
+        //}
+    }
+}
