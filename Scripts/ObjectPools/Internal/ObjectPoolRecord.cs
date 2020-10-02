1bf571ee3aa6cf817433e96dfc5df5aec5929dda
litsungyi <litsungyi@gmail.com>
2020-10-02 22:38:28 +0800

    feat: Update ObjectPool

---
+++ Scripts/ObjectPools/Internal/ObjectPoolRecord.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Camus.ObjectPools.Internal
+{
+    internal class ObjectPoolRecord : MonoBehaviour
+    {
+        private ObjectPool source;
+
+        internal void Create(ObjectPool pool)
+        {
+            source = pool;
+        }
+
+        internal void Release(ObjectPool target)
+        {
+            if (target != source)
+            {
+                Debug.LogError("Release pooled instance to different pool!");
+                Debug.LogWarning($"Source: {source.name}!", source);
+                Debug.LogWarning($"Target: {target.name}!", target);
+            }
+        }
+    }
+}
--- Scripts/ObjectPools/ObjectPool.cs
+++ Scripts/ObjectPools/ObjectPool.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Camus.ObjectPools.Internal;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Camus.ObjectPools
 {
     public class ObjectPool : MonoBehaviour
     {
+        [SerializeField]
+        private bool fixedSize;
+
         [SerializeField]
         private GameObject prefab;
 
@@ -18,28 +23,85 @@ namespace Camus.ObjectPools
         {
             for (int i = 0; i < minInstance; ++i)
             {
-                var instance = CreateInstance();
-                Release(instance);
+                var instance = Instantiate(prefab, transform);
+                AddDebugInfo(instance);
+                instance.SetActive(false);
+                reusables.Push(instance);
             }
         }
 
+        #region Debug
+
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        private void AddDebugInfo(GameObject instance)
+        {
+            var record = instance.AddComponent<ObjectPoolRecord>();
+            record.Create(this);
+        }
+
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        private void CheckDebugInfo(GameObject instance)
+        {
+            var record = instance.GetComponent<ObjectPoolRecord>();
+            if (record == null)
+            {
+                Debug.LogError("Pooled instance error!");
+            }
+            else
+            {
+                record.Release(this);
+            }
+        }
+
+        #endregion
+
         private GameObject CreateInstance()
         {
+            if (fixedSize)
+            {
+                Debug.LogError("Fixed ObjectPool cannot growth.");
+                return null;
+            }
+
             var instance = Instantiate(prefab, transform);
+            AddDebugInfo(instance);
             return instance;
         }
 
-        public GameObject Require()
+        public GameObject Require(Transform parent = null)
         {
             var instance = reusables.Any() ? reusables.Pop() : CreateInstance();
-            instance.SetActive(true);
+            instance?.SetActive(true);
+            if (parent != null)
+            {
+                instance?.transform.SetParent(parent);
+            }
+
             return instance;
+        }
+
+        public T Require<T>(Transform parent = null)
+            where T : MonoBehaviour
+        {
+            var instance = Require(parent);
+            var target = instance.GetComponent(typeof(T));
+            return target as T;
         }
 
         public void Release(GameObject instance)
         {
+            Assert.IsNotNull(instance);
+
             instance.SetActive(false);
             reusables.Push(instance);
+            instance.transform.SetParent(transform);
+            CheckDebugInfo(instance);
+        }
+
+        public void Release<T>(T instance)
+            where T : MonoBehaviour
+        {
+            Release(instance.gameObject);
         }
     }
 }
