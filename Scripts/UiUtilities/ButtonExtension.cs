1a091076d2e42ea396df946a7ef4dd5e8b7fffd3
litsungyi <litsungyi@gmail.com>
2020-10-07 01:54:44 +0800

    feat: Add Button Extension

---
+++ Scripts/UiUtilities/ButtonExtension.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Camus.UiUtilities
+{
+    public static class ButtonExtension
+    {
+        public static void SetColor(this Button button, Color color)
+        {
+            button.image.color = color;
+        }
+    }
+}
