de0c3d69790881bd7d3fb0b520cd3df94ce9b964
litsungyi <litsungyi@gmail.com>
2022-04-24 03:33:06 +0800

    refactor: Refactor style

--- Scripts/EditorHelpers/GizmoHelper.cs
+++ Scripts/EditorHelpers/GizmoHelper.cs
@@ -1,7 +1,7 @@
-using UnityEditor;
-using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
+using UnityEngine;
 
 namespace Camus.EditorHelpers
 {
--- Scripts/EventSystems/EventSource.cs
+++ Scripts/EventSystems/EventSource.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Camus.EventSystems
 {
